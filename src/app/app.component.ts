c51a07ca37676890c2f0078f84a1a3ba465955d4
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-07-04 19:01:01 -0700

    ng-gsap-scrollanimation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scrollanimation';
+export class AppComponent implements OnInit{
+  // sction one
+  @ViewChild('centerBox', {static:true}) centerBox:ElementRef<HTMLDivElement>
+
+  
+  constructor(){
+  }
+  ngOnInit(){
+  }
 }
+
